581130bf452bf3081b90775b56137e9ee70623a7
VusimuziAndroid <vsngwenya15@gmail.com>
2016-09-06 11:32:32 +0200

    local commits

---
+++ app/src/main/java/android/gitcommits/Person.java
@@ -0,0 +1,25 @@
+package android.gitcommits;
+
+/**
+ * Created by ABVN237 on 9/6/2016.
+ */
+public class Person {
+   private String name;
+   private String country;
+   private String twitter;
+
+    Person(String name,String country,String twitter){
+        this.name=name;
+        this.country=country;
+        this.twitter=twitter;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public String getCountry(){
+        return this.country;
+    }
+    public String getTwitter(){
+        return this.twitter;
+    }
+}
